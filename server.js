6f09b206598d28198a8d758117e75d5dd224b24d
Rob Kennedy <63457697+ciderbet@users.noreply.github.com>
2020-07-15 17:04:43 +0100

    Change server url

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 app.use(cors({ origin: process.env.CLIENT_URL }))
 
 // middleware
-app.use('/', saveDetails)
+app.use('/api', saveDetails)
 
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`API is running on port ${port}`))
