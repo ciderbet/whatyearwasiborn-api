bd7073db762b2b9b2450c92f8372480415305f62
Rob Kennedy <63457697+ciderbet@users.noreply.github.com>
2020-08-25 21:13:04 +0100

    upload images to S3 using form data

--- controllers/category.js
+++ controllers/category.js
@@ -1,7 +1,67 @@
 const Category = require('../models/category')
 const slugify = require('slugify')
+const formidable = require('formidable')
+const uuidv4 = require('uuid/v4')
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+// s3
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+})
 
 exports.create = (req, res) => {
+  const form = new formidable.IncomingForm()
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not upload'
+      })
+    }
+    console.table({ err, fields, files })
+    const { name, content } = fields
+    const { image } = files
+
+    const slug = slugify(name)
+    const category = new Category({ name, content, slug })
+
+    if (image.size > 2000000) {
+      return res.status(400).json({
+        error: 'Image should be less than 2mb'
+      })
+    }
+    // upload image to s3
+    const params = {
+      Bucket: 'whatyearwasiborn',
+      Key: `category/${uuidv4()}`,
+      Body: fs.readFileSync(image.path),
+      ACL: 'public-read',
+      ContentType: 'image/jpg'
+    }
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.status(400).json({ error: 'Upload to s3 failed' })
+      }
+      console.log('AWS UPLOAD RES DATA', data)
+      category.image.url = data.Location
+      category.image.key = data.Key
+
+      // save to db
+      category.save((err, success) => {
+        if (err) {
+          console.log(err)
+          res.status(400).json({ error: 'Category name already used' })
+        }
+        return res.json(success)
+      })
+    })
+  })
+}
+
+/* exports.create = (req, res) => {
   const { name, content } = req.body
   const slug = slugify(name)
   const image = {
@@ -21,7 +81,7 @@ exports.create = (req, res) => {
     }
     res.json(data)
   })
-}
+} */
 
 exports.list = (req, res) => {
 
